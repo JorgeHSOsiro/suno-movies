7be9f4990bdce50b7f6b4b7ef08d395281b5f7c5
jorgehsosiro <jojimurakabe@gmail.com>
2021-05-11 02:10:01 -0300

    showing catalogo completo

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { AiFillStar } from 'react-icons/ai';
+import { BiPlayCircle } from 'react-icons/bi';
 
 import styles from './card.module.scss';
 
-export function Cards(thumb, title, category, rate) {
+const IMG_API = 'https://image.tmdb.org/t/p/w1280';
+
+export function Cards({ thumb, title, category, rate, description }) {
   return (
     <div className={styles.cardContainer}>
-      <img src={thumb} alt={`thumbnail de ${title}`} />
-      <h3>{title}</h3>
-      <p>{category}</p>
-      <AiFillStar /> <p>{rate}</p>
+      <div className={styles.imageContainer}>
+        <img src={IMG_API + thumb} alt={`thumbnail de ${title}`} />
+        <BiPlayCircle className={styles.playMovie} />
+      </div>
+      <div>
+        <h3>{title}</h3>
+        <p className={styles.category}>{category}</p>
+        <div className={styles.rateContent}>
+          <AiFillStar className={styles.star} />
+          <p> {rate}</p>
+        </div>
+        <div className={styles.descContent}>
+          <p>{description}</p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Catalog/index.js
+++ src/components/Catalog/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { Cards } from '../Cards';
 import styles from './catalog.module.scss';
 
 export function Catalog() {
+  const [movies, setMovies] = useState([]);
+  const [halfMovies, setHalfMovies] = useState([]);
+  const [show, setShow] = useState(false);
+  const [category, setCategory] = useState('');
+  const [layout, setLayout] = useState('');
+  const simpleArray = ['por gênero', '1', '2', '3', '4', '5'];
+
+  useEffect(() => {
+    fetch(
+      'https://api.themoviedb.org/3/discover/movie?api_key=38a7c1e49c7c6930df26d2861bbe98a9&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate'
+    )
+      .then((result) => result.json())
+      .then((data) => {
+        console.log(data.results);
+        setMovies(data.results);
+        setHalfMovies(data.results.slice(0, 6));
+      });
+  }, []);
+
+  const showMore = () => {
+    setShow(true);
+  };
+
   return (
     <div id="catalogo" className={styles.catalogContainer}>
       <header className={styles.headerCatalog}>
@@ -12,15 +35,57 @@ export function Catalog() {
         </h1>
       </header>
       <main className={styles.mainContainer}>
-        <section>
-          <div>
-            <button type="button">por gênero</button>
+        <section className={styles.buttonSection}>
+          <div className={styles.firstBtnContainer}>
+            <select
+              className={styles.selectOpt}
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              {simpleArray.map((item) => (
+                <option value={item}>{item}</option>
+              ))}
+            </select>
             <button type="button">mais populares</button>
           </div>
-          <button type="button">Em lista</button>
+          <div>
+            <select
+              className={styles.selectOpt}
+              value={layout}
+              onChange={(e) => setLayout(e.target.value)}
+            >
+              <option value="emlista">em lista</option>
+              <option value="emgrid">em grid</option>
+            </select>
+          </div>
         </section>
-        <section>
-          <div>Lista de filmes</div>
+        <section className={styles.listSection}>
+          <div className={styles.cardsContainer}>
+            {show
+              ? movies.map((movie) => (
+                  <Cards
+                    key={movie.id}
+                    thumb={movie.poster_path}
+                    title={movie.title}
+                    category={movie.genre_ids}
+                    rate={movie.vote_average}
+                    description={movie.overview}
+                  />
+                ))
+              : halfMovies.map((movie) => (
+                  <Cards
+                    key={movie.id}
+                    thumb={movie.poster_path}
+                    title={movie.title}
+                    category={movie.genre_ids}
+                    rate={movie.vote_average}
+                    description={movie.overview}
+                  />
+                ))}
+          </div>
+          <button type="button" onClick={() => showMore()}>
+            carregar mais
+          </button>
         </section>
       </main>
     </div>
